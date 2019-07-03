95f262908be4c8c1c03c9211a077b7bfeafdb264
Lawrence Elango <lawrence.elango@pethersolutions.com>
2019-07-03 14:30:42 +0100

    file not loading

--- resources/views/freelancer.blade.php
+++ resources/views/freelancer.blade.php
@@ -13,7 +13,7 @@
 		href="{{ asset('assets/frontend/css/register/fonts/font-awesome-4.7.0/css/font-awesome.min.css')}}">
 	<!--===============================================================================================-->
 	<link rel="stylesheet" type="text/css"
-		href="{{ asset('assets/frontend/css/register/fonts/Linearicons-Free-v1.0.0/icon-font.min.css')}}">
+		href="{{ asset('assets/frontend/css/login/fonts/Linearicons-Free-v1.0.0/icon-font.min.css')}}">
 	<!--===============================================================================================-->
 	<link rel="stylesheet" type="text/css" href="{{ asset('assets/frontend/css/register/vendor/animate/animate.css')}}">
 	<!--===============================================================================================-->
