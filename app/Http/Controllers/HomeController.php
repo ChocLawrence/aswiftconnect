c886f68d07f4ad2852f548d1b21d2cb332d9d2ef
Lawrence Elango <lawrence.elango@pethersolutions.com>
2019-07-12 20:35:30 +0100

    fixes

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use App\Category;
 use App\Post;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -21,9 +23,11 @@ class HomeController extends Controller
      */
     public function index()
     {
+        $userId=Auth::user()->role_id;
+        Log::info($userId);
         $categories = Category::all();
         $posts = Post::latest()->approved()->published()->take(6)->get();
-        return view('home',compact('categories','posts'));
+        return view('home',compact('categories','posts','userId'));
     }
 
 }
