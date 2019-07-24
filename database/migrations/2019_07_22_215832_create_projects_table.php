42264dc287ab48165bf5213df4e96048b7dae1b1
Lawrence Elango <lawrence.elango@pethersolutions.com>
2019-07-24 01:00:45 +0100

    ++

--- database/migrations/2019_07_22_215832_create_projects_table.php
+++ database/migrations/2019_07_22_215832_create_projects_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateFreelancerProjectsTable extends Migration
+class CreateProjectsTable extends Migration
 {
     /**
      * Run the migrations.
