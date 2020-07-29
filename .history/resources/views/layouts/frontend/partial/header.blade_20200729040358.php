2ea1dbb8a301a82f8cb0392296c87ad995623e1b
Lawrence Elango <lawrence@MI.local>
2020-07-29 04:05:02 +0100

    +=

--- .history/resources/views/layouts/frontend/partial/header.blade_20200729040358.php
+++ .history/resources/views/layouts/frontend/partial/header.blade_20200729040358.php
@@ -6,8 +6,8 @@
         <div class="menu-nav-icon" data-nav-menu="#main-menu"><i class="ion-navicon"></i></div>
 
         <ul class="main-menu visible-on-click" id="main-menu">
-            <li><a href="{{ route('home') }}">Projects</a></li>
-            <li><a href="{{ route('post.index') }}">Posts</a></li>
+            {{-- <li><a href="{{ route('home') }}">Projects</a></li>
+            <li><a href="{{ route('post.index') }}">Posts</a></li> --}}
             {{--<li><a href="{{ route('browse') }}">Freelancers</a></li>--}}
             @guest
                 <li><a href="{{ route('login') }}">Login</a></li>
